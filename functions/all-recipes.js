65b05f4e0038097ac7f83f02f226b1291f3a7eeb
ttntm <ttntm@pm.me>
2021-03-19 14:32:17 +0100

    add: initial config

---
+++ functions/all-recipes.js
@@ -0,0 +1,33 @@
+const faunadb = require('faunadb');
+
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET
+})
+
+exports.handler = (event, context, callback) => {
+  console.log("Function `all-recipes` invoked")
+  return client.query(q.Paginate(q.Match(q.Ref("indexes/all_recipes"))))
+  .then((response) => {
+    const recipeRefs = response.data
+    console.log("Recipe refs", recipeRefs)
+    console.log(`${recipeRefs.length} recipes found`)
+    // create new query out of recipe refs. http://bit.ly/2LG3MLg
+    const getAllRecipeDataQuery = recipeRefs.map((ref) => {
+      return q.Get(ref)
+    })
+    // then query the refs
+    return client.query(getAllRecipeDataQuery).then((ret) => {
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(ret)
+      })
+    })
+  }).catch((error) => {
+    console.log("error", error)
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    })
+  })
+}
