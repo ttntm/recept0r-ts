c719f7ad2439d8f44c3afc4573adb35bfe65ca44
ttntm <ttntm@pm.me>
2021-04-21 22:59:01 +0200

    update: deps

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,31 @@
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default {
-  plugins: [vue()],
-};
+export default defineConfig({
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      // Disabled Hashing as Netlify Does Hashing for us using Etag.
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      }
+    }
+  },
+  optimizeDeps: {
+    include: [
+      '@tiptap/vue-3',
+      'gotrue-js',
+      'secure-ls',
+      'vue',
+      'vue-router',
+      'vuex',
+      'vuex-multi-tab-state',
+      'vuex-persistedstate'
+    ]
+  },
+  plugins: [
+    vue()
+  ]
+});
