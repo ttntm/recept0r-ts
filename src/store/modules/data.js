ttntm/recept0r-ts

commit 57b04fb96d7fd8dabfadec2c5b82d10dd73c9573

update: less redundancy, types and store data

src/store/modules/data.js
@@ -1,4 +1,4 @@
-import { apiRequest } from '@/utils'
+import { apiRequest, getArrayIndex } from '@/utils'
 
 export default {
   strict: false,
@@ -124,30 +124,25 @@ export default {
       
       commit('SET_FILTER_DATA', Object.assign({}, fData, newFilterData)) // MUST use _new_ object here; kills reactivity otherwise!
       
-      const findIndex = (arr, el) => {
-        if (!arr) return -1
-        return arr.length === 0 ? 0 : arr.indexOf(el)
-      }
-      
       const doFilter = (input) => {
         let fDataLength = Object.keys(fData).length
 
         return input.filter((item) => {
           // prepare the data
-          let cat = findIndex(fData.category, item.data.category.toLowerCase())
-          let dt = findIndex(fData.diet, item.data.diet.toLowerCase())
+          let cat = getArrayIndex(fData.category, item.data.category)
+          let dt = getArrayIndex(fData.diet, item.data.diet)
           // handle the possibilities
           switch(fDataLength) {
             case 2:
               // fDataLength = 2 -- BOTH conditions are set
-              if(cat !== -1 && dt !== -1) {
+              if (cat !== -1 && dt !== -1) {
                 return true
               }
               break
 
             case 1:
               // fDataLength = 1 -- ONE condition is set
-              if(cat !== -1 || dt !== -1) {
+              if (cat !== -1 || dt !== -1) {
                 return true
               }
               break

src/types.d.ts
@@ -1,7 +1,10 @@
 import type { UserData } from 'gotrue-js'
 
-export type Credentials = {
-  [CKey: string]: string
+export interface GenObj {
+  [OKey: string]: any
+}
+
+export interface Credentials extends GenObj {
   name?: string
   email: string
   password: string
@@ -18,14 +21,12 @@ export type Headers = {
   'Content-Type': string
 }
 
-export type FilterSelection = {
-  [FKey: string]: string[]
+export interface FilterSelection extends GenObj {
   category: string[]
   diet: string[]
 }
 
-export type Recipe = {
-  [RKey: string]: any
+export interface Recipe extends GenObj {
   id: string
   draft?: boolean
   title: string

src/utils/index.ts
@@ -29,6 +29,7 @@ export async function apiRequest(reqMethod: string, payload?: any, reqPath: stri
  * Returns the index of an element in the given array
  */
 export function getArrayIndex(arr: string[], item: string) {
+  if (!arr) return -1
   const target: string[] = [...arr]
   return target.indexOf(item.toLowerCase())
 }

commit e7302964d09367ec26d01bd122bcb0c8d246b5dd

fix: regression introduced by commit 57b04fb96d7fd8dabfadec2c5b82d10dd73c9573

src/store/modules/data.js
@@ -128,10 +128,14 @@ export default {
         let fDataLength = Object.keys(fData).length
 
         return input.filter((item) => {
-          // prepare the data
-          let cat = getArrayIndex(fData.category, item.data.category)
-          let dt = getArrayIndex(fData.diet, item.data.diet)
-          // handle the possibilities
+          let cat = fData.category.length === 0
+            ? 0
+            : getArrayIndex(fData.category, item.data.category)
+          
+          let dt = fData.diet.length == 0
+            ? 0
+            : getArrayIndex(fData.diet, item.data.diet)
+          
           switch(fDataLength) {
             case 2:
               // fDataLength = 2 -- BOTH conditions are set